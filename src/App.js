75e0fac70d8ee229b084af8d69f240b8e3c70875
Malaka Samarasinghage <malaka@Malakas-MacBook-Pro.local>
2019-10-19 15:56:18 +1100

    component update lifecycles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+  //Component creation lifecycles
 
   //Constructor is default ES6 feature
   constructor(props) {
@@ -20,17 +21,11 @@ class App extends Component {
     return state;                                           //Return state
   }
 
-  shouldComponentUpdate() {
-    console.log("shouldComponentUpdate");
+  shouldComponentUpdate(nextProps, nextState) {             //Last minute DOM updates like user scrolling position
+    console.log("shouldComponentUpdate", nextProps, nextState);
     return true;
   }
 
-  componentDidMount() {
-    console.log("ComponentDidMount called")
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
 
   render() {                                  //Cause side-effects
     console.log("In Render ", this.state);    //Don't call set state immediately or synchronously
@@ -40,6 +35,29 @@ class App extends Component {
       </div>
     );
   }
+
+  //Update child components
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate ", prevProps, prevState);
+    return {message: 'smoe data snapshot'};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot){               //Can cause side-effects
+    console.log("componentDidUpdate", snapshot);
+  }
+
+  componentDidMount() {
+    console.log("ComponentDidMount called")
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  //Component Updating lifecycles
+
+
+
 }
 
 export default App;
