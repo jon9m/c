7df05a29c363b9504e7766bf5b8d5d399ddc4c24
Malaka Samarasinghage <malaka@Malakas-MacBook-Pro.local>
2019-10-19 15:27:08 +1100

    component creation lifecycles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  render() {
+
+  //Constructor is default ES6 feature
+  constructor(props) {
+    super(props);
+
+    this.state = {        //Setup state
+      count: 0
+    };                    //No side-effects
+
+    console.log("Contructor called");
+  }
+
+  static getDerivedStateFromProps(props, state) {           //Static
+    console.log("getDerivedStateFormProps");
+    state = { ...state, count: 4 };                         //Sync state
+    return state;                                           //Return state
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidMount() {
+    console.log("ComponentDidMount called")
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  render() {                                  //Cause side-effects
+    console.log("In Render ", this.state);    //Don't call set state immediately or synchronously
     return (
       <div>
-        
+
       </div>
     );
   }
